ab48c7e7b18077fed345b23831f5a553072f1b2c
Ryusuke Kimura <acenazuna@gmail.com>
2019-05-15 18:30:56 +0900

    [Start]3-9

---
+++ vagrant/sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'f4939118720147a590912365d3186534af4ea680886efdd48d8864dbe65436218cc74201406ca89a45136d3c5bb2fdf397588fc52ff9a98b85d7b9152e9511d7'
